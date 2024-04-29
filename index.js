bd6eff321999ad4dce68d4c45a0d122069c939ac
airdropchest <airdrop.chest@gmail.com>
2024-04-29 09:15:50 +0200

    world

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ console.log(string)
 
 alert(string)
 setTimeout(() => location.href = "https://codeshack.io", 10000);
+function capitalizeWords(str) {
+  return str.replace(/\w/g, function(l){ return l.toUpperCase() });
+}
