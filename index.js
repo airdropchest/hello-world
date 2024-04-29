28797641ca65b3fb025eb2127228259a6ff7bd87
airdropchest <airdrop.chest@gmail.com>
2024-04-29 09:18:25 +0200

    Mon Apr 29 09:18:25 CEST 2024

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ setTimeout(() => location.href = "https://codeshack.io", 10000);
 function capitalizeWords(str) {
   return str.replace(/\w/g, function(l){ return l.toUpperCase() });
 }
+if (/\S+@\S+\.\S+/.test('david@codeshack.io')) {
+    console.log('Email is valid!');
+} else {
+    console.log('Email is invalid!');
+}
+const currentUrl = window.location.href;
