cbcec535b4e35c2e8212d8484ad68aca44a4014d
airdropchest <airdrop.chest@gmail.com>
2024-05-08 10:06:32 +0200

    Wed May  8 10:06:32 CEST 2024

--- index.js
+++ index.js
@@ -37,3 +37,6 @@ const isRippleAddress = (address) => {
   return regex.test(address);
 }
 let randomNum = Math.floor(Math.random() * maxNum);
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
