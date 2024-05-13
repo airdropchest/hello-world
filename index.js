8bac719467e6b39c36c8905c910b5ea6ca6f3155
airdropchest <airdrop.chest@gmail.com>
2024-05-13 15:33:56 +0200

    Mon May 13 15:33:56 CEST 2024

--- index.js
+++ index.js
@@ -48,3 +48,15 @@ function toTitleCase(str) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
+const isPostalCode = (postalCode, countryCode) => {
+  if (countryCode === 'US') {
+    const regex = /[0-9]{5}(?:-[0-9]{4})?/;
+    return regex.test(postalCode);
+  } else if (countryCode === 'CA') {
+    const regex = /[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/;
+    return regex.test(postalCode.toUpperCase());
+  } else {
+    // Add regex for other country codes as needed
+    return false;
+  }
+}
