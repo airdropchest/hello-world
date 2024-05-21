3c59370e60e9e5246d54c6237ca566efeaa7672c
airdropchest <airdrop.chest@gmail.com>
2024-05-21 07:10:38 +0200

    Tue May 21 07:10:38 CEST 2024

--- index.js
+++ index.js
@@ -82,3 +82,15 @@ window.scroll({
     behavior: 'smooth' 
 });
 const array = [...Array(10).keys()].map(i => i + 1);
+const isPostalCode = (postalCode, countryCode) => {
+  if (countryCode === 'US') {
+    const regex = /[0-9]{5}(?:-[0-9]{4})?/;
+    return regex.test(postalCode);
+  } else if (countryCode === 'CA') {
+    const regex = /[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] [0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/;
+    return regex.test(postalCode.toUpperCase());
+  } else {
+    // Add regex for other country codes as needed
+    return false;
+  }
+}
