982990c1d23713ede734b066e4c56deca46abbe8
airdropchest <airdrop.chest@gmail.com>
2024-06-08 08:50:54 +0200

    Sat Jun  8 08:50:54 CEST 2024

--- index.js
+++ index.js
@@ -215,3 +215,14 @@ const element = document.querySelector('#element');
 if (element.offsetParent === null) {
     console.log('Element is hidden');
 }
+function countdownTimer(minutes) {
+  let seconds = minutes * 60;
+  const countdown = setInterval(function() {
+    seconds--;
+    if (seconds < 0) {
+      clearInterval(countdown);
+    } else {
+      console.log(seconds + " seconds left");
+    }
+  }, 1000);
+}
