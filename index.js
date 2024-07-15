cdd5c193b7a8eaee923b4161ba6a917eb9178e09
airdropchest <airdrop.chest@gmail.com>
2024-07-15 10:43:11 +0200

    Mon Jul 15 10:43:11 CEST 2024

--- index.js
+++ index.js
@@ -438,3 +438,6 @@ const isRGBColor = (rgb) => {
   const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
   return regex.test(rgb);
 }
+let newArr = oldArr.map(function(item) {
+  return item + 1;
+});
