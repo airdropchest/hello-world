6d337fe1f44da5ea82405dedec9a14a8adeea74f
airdropchest <airdrop.chest@gmail.com>
2024-07-15 08:34:35 +0200

    Mon Jul 15 08:34:35 CEST 2024

--- index.js
+++ index.js
@@ -434,3 +434,7 @@ function capitalizeWords(str) {
 function sortByProperty(arr, property) {
   return arr.sort((a, b) => (a[property] > b[property]) ? 1 : -1);
 }
+const isRGBColor = (rgb) => {
+  const regex = /rgb\(\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*,\s*([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\s*\)/;
+  return regex.test(rgb);
+}
