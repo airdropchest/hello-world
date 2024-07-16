c1ee5334b5aff065ff8179ab00fc541b586e1c08
airdropchest <airdrop.chest@gmail.com>
2024-07-16 08:52:21 +0200

    Tue Jul 16 08:52:21 CEST 2024

--- index.js
+++ index.js
@@ -460,3 +460,16 @@ const element = document.querySelector('#element');
 if (element.offsetParent === null) {
     console.log('Element is hidden');
 }
+const onScrollStop = callback => {
+  let isScrolling;
+  window.addEventListener(
+    'scroll',
+    e => {
+      clearTimeout(isScrolling);
+      isScrolling = setTimeout(() => {
+        callback();
+      }, 150);
+    },
+    false
+  );
+};
