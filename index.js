386ab223696654de7153209768ce5675726e0145
airdropchest <airdrop.chest@gmail.com>
2024-08-30 02:02:56 +0200

    Fri Aug 30 02:02:56 CEST 2024

--- index.js
+++ index.js
@@ -683,3 +683,14 @@ const debounce = (fn, time) => {
     timeout = setTimeout(() => fn.apply(this, args), time);
   };
 };
+fetch('https://example.com/authenticate', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username: 'David', password: '12345' })
+}).then(response => response.json()).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error('Error:', error);
+});
