96f487649a03d30637708fd1499301e8e4ceec20
airdropchest <airdrop.chest@gmail.com>
2024-09-03 07:18:32 +0200

    Tue Sep  3 07:18:32 CEST 2024

--- index.js
+++ index.js
@@ -709,3 +709,6 @@ function toTitleCase(str) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
+}
