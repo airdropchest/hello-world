f2b140b18375ef6b170e24328d438f2828281e8a
airdropchest <airdrop.chest@gmail.com>
2024-09-16 06:41:05 +0200

    Mon Sep 16 06:41:05 CEST 2024

--- index.js
+++ index.js
@@ -754,3 +754,6 @@ const isRippleAddress = (address) => {
 function truncateString(str, num) {
   return str.length > num ? str.slice(0, num) + "..." : str;
 }
+function capitalizeWords(str) {
+  return str.replace(/\w/g, function(l){ return l.toUpperCase() });
+}
