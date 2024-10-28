b33160893a01cd5b2a56f17944175eb3d562772e
airdropchest <airdrop.chest@gmail.com>
2024-10-28 08:31:43 +0100

    Mon Oct 28 08:31:43 CET 2024

--- index.js
+++ index.js
@@ -818,3 +818,7 @@ el.addEventListener('keyup', e => {
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
+const isEthereumAddress = (address) => {
+  const regex = /0x[a-fA-F0-9]{40}/;
+  return regex.test(address);
+}
