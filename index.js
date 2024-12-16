48a0e3b95a3af29ce3c304f010f287ebd972ddcb
airdropchest <airdrop.chest@gmail.com>
2024-12-16 06:47:09 +0100

    Mon Dec 16 06:47:08 CET 2024

--- index.js
+++ index.js
@@ -861,3 +861,14 @@ function toTitleCase(str) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
+fetch('https://example.com/authenticate', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username: 'David', password: '12345' })
+}).then(response => response.json()).then(data => {
+    console.log(data);
+}).catch(error => {
+    console.error('Error:', error);
+});
